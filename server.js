372941ee39d6494f9bde9c10c5e88273fbe41b44
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-10-14 13:40:20 -0300

    add user json

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("data/movies.json");
+const routerMovies = jsonServer.router("data/movies.json");
+const routerUsers = jsonServer.router("data/users.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
 server.use(middlewares);
-server.use(router);
+server.use(routerMovies);
+server.use(routerUsers);
 
 server.listen(port);

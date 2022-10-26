dc7cd2737030cfbf0bdf2f604a8cefe26fa62245
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-10-26 19:31:45 -0300

    new scripts and lists

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
-const data = require("./database.json");
+const data = require("./lists/series.json");
 const fs = require("fs");
 
 console.log("startou");
-console.log(data.users);
+// console.log(data.users);
 
-// ADD ID
-// let id = 1;
-// data.movies.forEach((movie) => {
-//   movie.id = id;
-//   id++;
+// let id = 20000;
+// data.series.forEach((media) => {
+// let length = media.name.length;
+// media.id = id;
+// media.type = "series";
+// let index = length - 2;
+// if (parseInt(media.name.substring(length - 2)) == NaN) {
+//   index = length - 1;
+// }
+// media.episode = parseInt(media.name.substring(index));
+// media.name = media.name.substring(0, index - 2);
+//
+// length = media.name.length;
+// index = length - 2;
+// if (parseInt(media.name.substring(length - 2)) == NaN) {
+//   index = length - 1;
+// }
+// media.season = parseInt(media.name.substring(index));
+// media.name = media.name.substring(0, index - 2);
+// id++;
 // });
 
-data.movies.forEach((movie) => {
-  delete movie.tvg;
+// =========================================================================================== GROUP BY
+// let groupByName = data.series.reduce((entryMap, e) => {
+//   return entryMap.set(e.name, [...(entryMap.get(e.name) || []), e]);
+// }, new Map());
+//
+// let groupByEpisodes = Array.from(groupByName).map((media) => {
+//   return {
+//     name: media[0],
+//     episodes: media[1],
+//   };
+// });
+//
+// let groupBySeason = groupByEpisodes.map((media) => {
+//   let seasonMap = Array.from(
+//     media.episodes.reduce((entryMap, e) => {
+//       return entryMap.set(e.season, [...(entryMap.get(e.season) || []), e]);
+//     }, new Map())
+//   );
+//
+//   let result = {
+//     name: media.name,
+//     seasons: seasonMap.map((season) => {
+//       return {
+//         season: season[0],
+//         episodes: season[1],
+//       };
+//     }),
+//   };
+//
+//   return result;
+// });
+
+let i = 20653;
+data.series.forEach((media) => {
+  media.forEach((serie) => {
+    console.log(serie.name);
+    serie.id = i;
+    serie.seasons.map((season) => {
+      season.episodes.map((episode) => {
+        // console.log(season.season);
+        // serie.category = episode.category;
+        delete episode.name;
+      });
+    });
+    // console.log(serie.name);
+  });
+  i++;
 });
 
 let newData = JSON.stringify(data);
 
-fs.writeFile("database.json", newData, "utf-8", () => {
+fs.writeFile("./lists/series_.json", newData, "utf-8", () => {
   console.log("-------- fim");
 });
